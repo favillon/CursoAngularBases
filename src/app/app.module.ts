fd1d860dfb9626298a693b00d6e9da28da908af0
favillon <favillon@hotmail.com>
2021-10-14 00:00:41 -0500

    Fin seccion 4 Introduccion a angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContadorComponent } from './contador/contador.component';
+
+import { HeroesModule } from './heroes/heores.module';
+//import { HeroeComponent } from './heroes/heore/heroe.component';
+//import { ListadoComponent } from './heroes/listado/listado.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContadorComponent,
+    //HeroeComponent,
+    //ListadoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
